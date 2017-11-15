c0dd64f7e026d5f4ff4f79d19e9c3aaa3bb7daf1
baris <baris.aydinoz@gmail.com>
2017-11-15 12:34:01 +0300

    client config from input stream

--- src/test/java/com/hazelcast/k8s/ci/DiscoveryAcceptanceTest.java
+++ src/test/java/com/hazelcast/k8s/ci/DiscoveryAcceptanceTest.java
@@ -1,11 +1,16 @@
 package com.hazelcast.k8s.ci;
 
 import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.XmlClientConfigBuilder;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,8 +19,11 @@ public class DiscoveryAcceptanceTest {
     private HazelcastInstance client;
 
     @Before
-    public void before() {
-        client = HazelcastClient.newHazelcastClient();
+    public void before() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream stream = classLoader.getResource("hazelcast-client.xml").openStream();
+        ClientConfig cfg = new XmlClientConfigBuilder(stream).build();
+        client = HazelcastClient.newHazelcastClient(cfg);
     }
 
     @Test
